4352182d4a740016ce7a91a9f2712cec68be1b38
Greenville Associates Consulting <55986791+greenvilleassociates@users.noreply.github.com>
2025-11-01 21:21:29 -0400

    Update DirtbikeContext.cs

--- Data/DirtbikeContext.cs
+++ Data/DirtbikeContext.cs
@@ -74,7 +74,8 @@ public partial class DirtbikeContext : DbContext
 
     protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
 #warning To protect potentially sensitive information in your connection string, you should move it out of source code. You can avoid scaffolding the connection string by using the Name= syntax to read it from configuration - see https://go.microsoft.com/fwlink/?linkid=2131148. For more guidance on storing connection strings, see https://go.microsoft.com/fwlink/?LinkId=723263.
-        => optionsBuilder.UseSqlite("Data Source=./SQLDATA/dirtbike.db");
+        => optionsBuilder.UseSqlite("Data Source=D:\home\data\SQLDATA\dirtbike.db");
+
 
     protected override void OnModelCreating(ModelBuilder modelBuilder)
     {
