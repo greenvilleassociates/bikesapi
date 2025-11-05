940d7464114ed1a4c1a5f268d364d840bc495c50
Greenville Associates Consulting <55986791+greenvilleassociates@users.noreply.github.com>
2025-11-05 10:54:32 -0500

    Add files via upload

---
+++ Controllers/File.cs
@@ -0,0 +1,59 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using System;
+using System.Collections.Generic;
+using System.IO;
+
+namespace Enterprise.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class FileOpsController : ControllerBase
+    {
+        private readonly ILogger<FileOpsController> _logger;
+        private readonly string _localProfilePath = "/home/wwwroot/profiles";
+
+        public FileOpsController(ILogger<FileOpsController> logger)
+        {
+            _logger = logger;
+        }
+
+        [HttpGet(Name = "GetFileOps")]
+        public IEnumerable<FileMenu> Get()
+        {
+            var files = Directory.Exists(_localProfilePath)
+                ? Directory.GetFiles(_localProfilePath)
+                : Array.Empty<string>();
+
+            var fileMenus = new List<FileMenu>();
+
+            foreach (var filePath in files)
+            {
+                var fileName = Path.GetFileName(filePath);
+                fileMenus.Add(new FileMenu
+                {
+                    Date = DateOnly.FromDateTime(DateTime.Now),
+                    Filename = fileName,
+                    FilePath = filePath,
+                    BucketFullPath = filePath,
+                    SecurityToken = "local",
+                    BucketToken = "local",
+                    AzureBlobConnectionString = "local"
+                });
+            }
+
+            return fileMenus;
+        }
+    }
+
+    public class FileMenu
+    {
+        public DateOnly Date { get; set; }
+        public string Filename { get; set; }
+        public string FilePath { get; set; }
+        public string SecurityToken { get; set; }
+        public string BucketToken { get; set; }
+        public string BucketFullPath { get; set; }
+        public string AzureBlobConnectionString { get; set; }
+    }
+}
