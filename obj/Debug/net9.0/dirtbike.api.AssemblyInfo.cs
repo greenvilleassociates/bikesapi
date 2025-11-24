0d3d73885a26b8b2f70305a87548bcfb3a893998
Greenville Associates Consulting <55986791+greenvilleassociates@users.noreply.github.com>
2025-11-24 14:07:04 -0500

    Adding SomeComments

--- obj/Debug/net9.0/dirtbike.api.AssemblyInfo.cs
+++ obj/Debug/net9.0/dirtbike.api.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("dirtbike.api")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+d6f2fdf2b62e4825c5a24db55201e07d2be6d6f9")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+eb24b6ca88c3bc2ff31eae363be1966ff0351829")]
 [assembly: System.Reflection.AssemblyProductAttribute("dirtbike.api")]
 [assembly: System.Reflection.AssemblyTitleAttribute("dirtbike.api")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
