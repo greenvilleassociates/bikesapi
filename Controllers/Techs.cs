3aee76de9b316d50c53aa06adbbbd035945ffb9a
Greenville Associates Consulting <55986791+greenvilleassociates@users.noreply.github.com>
2025-11-29 10:26:04 -0500

    Update Techs.cs

--- Controllers/Techs.cs
+++ Controllers/Techs.cs
@@ -51,23 +51,45 @@ public static class NoctechsEndpoints
         .WithName("GetNoctechsById")
         .WithOpenApi();
 
-        //[HttpPut]
-        group.MapPut("/{id}", async (int id, Noctech input) =>
-        {
-            using (var context = new DirtbikeContext())
-            {
-                Noctech[] someNoctechs = context.Noctechs.Where(m => m.Id == id).ToArray();
-                context.Noctechs.Attach(someNoctechs[0]);
-                if (input.Employeeid != null) someNoctechs[0].Employeeid = input.Employeeid;
-                await context.SaveChangesAsync();
-                Enterpriseservices.ApiLogger.logapi(Enterpriseservices.Globals.ControllerAPIName, Enterpriseservices.Globals.ControllerAPINumber, "PUTWITHID", 1, "Test", "Test");
-                return TypedResults.Accepted("Updated ID:" + input.Id);
-            }
+group.MapPut("/{id}", async (int id, Noctech input) =>
+{
+    using (var context = new DirtbikeContext())
+    {
+        Noctech[] someNoctechs = context.Noctechs.Where(m => m.Id == id).ToArray();
 
+        if (someNoctechs.Length == 0)
+            return TypedResults.NotFound("No NOC tech found with ID: " + id);
 
-        })
-        .WithName("UpdateNoctechs")
-        .WithOpenApi();
+        context.Noctechs.Attach(someNoctechs[0]);
+
+        // ✅ Update all fields if provided
+        if (input.Userid != null) someNoctechs[0].Userid = input.Userid;
+        if (input.Employeeid != null) someNoctechs[0].Employeeid = input.Employeeid;
+        if (input.Email != null) someNoctechs[0].Email = input.Email;
+        if (input.Phone != null) someNoctechs[0].Phone = input.Phone;
+        if (input.Sms != null) someNoctechs[0].Sms = input.Sms;
+        if (input.Techaddress1 != null) someNoctechs[0].Techaddress1 = input.Techaddress1;
+        if (input.Techaddress2 != null) someNoctechs[0].Techaddress2 = input.Techaddress2;
+        if (input.Techcity != null) someNoctechs[0].Techcity = input.Techcity;
+        if (input.Techstate != null) someNoctechs[0].Techstate = input.Techstate;
+        if (input.Techzip != null) someNoctechs[0].Techzip = input.Techzip;
+
+        await context.SaveChangesAsync();
+
+        Enterpriseservices.ApiLogger.logapi(
+            Enterpriseservices.Globals.ControllerAPIName,
+            Enterpriseservices.Globals.ControllerAPINumber,
+            "PUTWITHID",
+            1,
+            "Updated NOC Tech",
+            "ID: " + id
+        );
+
+        return TypedResults.Accepted("Updated ID: " + id);
+    }
+})
+.WithName("UpdateNoctechs")
+.WithOpenApi();
 
         group.MapPost("/", async (Noctech input) =>
         {
