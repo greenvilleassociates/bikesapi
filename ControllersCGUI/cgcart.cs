e0baa2c77a819e9b0056b6231d4bb4969326a00e
Greenville Associates Consulting <55986791+greenvilleassociates@users.noreply.github.com>
2025-12-09 07:42:33 -0500

    Update cgcart.cs

--- ControllersCGUI/cgcart.cs
+++ ControllersCGUI/cgcart.cs
@@ -20,7 +20,7 @@ namespace Enterprise.Controllers
                 var cart = service.CreateCart(dto);
 
                 Enterpriseservices.ApiLogger.logapi("CGCartAPI", "005", "CGCREATE", 1, "Create", $"Cart {dto.Uid}");
-                return cart == null ? Results.BadRequest("User not found") : Results.Created($"/api/CGCart/{id}", cart);
+                return cart == null ? Results.BadRequest("User not found") : Results.Created($"/api/CGCart/1", cart);
             })
             .WithName("CGCreateCart")
             .WithOpenApi();
